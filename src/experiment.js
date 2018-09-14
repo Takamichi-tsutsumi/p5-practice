b0a502261c36b2e0e8f88e41c6bf92a8c36c92f4
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-09-14 18:57:28 +0900

    Hello world

--- src/experiment.js
+++ src/experiment.js
@@ -1,19 +1,15 @@
 import p5 from "p5";
 
 function sketch(p) {
-  p.setup = function() {
-    p.createCanvas(p.windowWidth, p.windowHeight);
-    p.background(0);
+  let x = 0;
+  p.setup = () => {
+    p.background(100);
   };
 
-  p.draw = function() {
-    if (p.mouseIsPressed) {
-      p.fill(0);
-    } else {
-      p.fill(255);
-    }
-    p.ellipse(p.mouseX, p.mouseY, 80, 80);
+  p.draw = () => {
+    p.ellipse(x, p.height / 2, 20, 20);
+    x = x + 1;
   };
 }
 
-var app = new p5(sketch, document.body);
+const app = new p5(sketch, document.body);
